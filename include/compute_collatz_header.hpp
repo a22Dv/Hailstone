a316296a71d9b331cf3b9586affce65e86923d92
a22Dv <a22dev.gl@gmail.com>
2025-04-27 19:51:20 +0800

    Project initialization.

---
+++ include/compute_collatz_header.hpp
@@ -0,0 +1,14 @@
+#ifndef COMPUTE_COLLATZ_HPP_
+#define COMPUTE_COLLATZ_HPP_
+#include <vector>
+#include <iostream>
+#include <string>
+#include <cstdint>
+class Collatz {
+private:
+    std::vector<int64_t> get_sequence(int32_t seed);
+    std::vector<std::string> splitString(std::string str, char delimiter=' ');
+public:
+    void main_process();
+};
+#endif
---
+++ src/compute_collatz.cpp
@@ -0,0 +1,7 @@
+#include "compute_collatz_header.hpp"
+
+int main(int argc, char* argv[]) {
+    Collatz c;
+    c.main_process();
+}
+
