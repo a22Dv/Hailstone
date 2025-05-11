9f95e82bef33dc161ef96cc907b8855b04d9e058
a22Dv <a22dev.gl@gmail.com>
2025-05-11 11:53:09 +0800

    Added sequence calculations for C++ subprocess.

--- include/collatz_header.hpp
+++ include/collatz_header.hpp
@@ -1,4 +1,32 @@
 #ifndef COLLATZ_HPP_
 #define COLLATZ_HPP_
 
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <string>
+#include <cstdint>
+#include <memory>
+
+class Application {
+private:
+    std::unique_ptr<Utilities> utilInstance = nullptr;
+    std::vector<uint64_t> getSequence(uint32_t n);
+public:
+    Application(const std::unique_ptr<Utilities> utils);
+    void start();
+};
+
+class Utilities {
+private:
+    const char messageStart = 'm';
+    const char resultStart = 'r';
+    const char endDelimiter = '/';
+    const char dividerNum = '.';
+    const char dividerSeq = '|';
+public:
+    std::string wrapMessage(const std::string &message);
+    std::string wrapSequences(const std::vector<std::vector<uint64_t>> &sequences);
+    std::vector<std::string> splitByChar(char c, const std::string &str); 
+};
 #endif
---
+++ src/py/main.py
@@ -0,0 +1,11 @@
+class Application:
+    def __init__(self) -> None:
+        pass
+
+    def start(self) -> None:
+        """Main orchestrator for the application."""
+        pass
+
+# Entry point.
+if __name__ == "__main__":
+    Application().start()
